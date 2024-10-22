80d060c925d88a1b0c678e752fcdc7a301d4344a
ParthBhatt011 <PARTHBHATT011@GMAIL.COM>
2024-10-22 21:15:11 +0530

    meme.cpp

---
+++ meme.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+
+int main(){
+
+    Animal =tota;
+    Animal =bakri;
+
+    string = user_input;
+
+    cout <<"Tota Bole :"
+    getline(cin, user_input);
+    
+    if (tota.bole(user_input) == "tae tae tae"){
+        cout<<bakri.bole("mhee..........!")<< end1;
+        this_thread::sleep_for( chrome::seconds)
+        cout<< bakri.bole("mhee........")<< end1;
+    }
+    else{
+        cout<<" The input does not match." <<end1;
+    }
+    return 0;
+}
